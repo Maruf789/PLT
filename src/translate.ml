add50aec93bf25cdcdf5cd89d899a2bb1e9fe591
mwcu <wm19922009@gmail.com>
2014-11-10 20:23:11 -0500

    t

--- src/translate.ml
+++ src/translate.ml
@@ -1,7 +1,7 @@
 open Ast
 
 let rec compile prg =
-  let funs = prg.pfuns in (* function table *)
+  (*let funs = prg.pfuns in (* function table *)*)
   let vars = prg.pvars in (* variable table *)
   let stms = prg.pstms in (* statement list *)
   print_endline "Good"
