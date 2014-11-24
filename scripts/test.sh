96add94fe9923d85d684b7679e4cb07b28ceee39
Lan Yang <ireneyanglan@gmail.com>
2014-11-23 22:11:02 -0500

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ SAMPLES_DIR=./test_samples
 OUTPUT_DIR=./test_outputs
 IDEAOUTPUT_DIR=./test_ideaoutputs
 
-$1 $SAMPLES_DIR/sample0.bc sample0.c > $OUTPUT_DIR/sample0out.txt
-$1 $SAMPLES_DIR/sample1.bc sample1.c > $OUTPUT_DIR/sample1out.txt
-$1 $SAMPLES_DIR/sample2.bc sample2.c > $OUTPUT_DIR/sample2out.txt
-$1 $SAMPLES_DIR/sample3.bc sample3.c > $OUTPUT_DIR/sample3out.txt
+for (( i =  0; i <= 5; i++))
+do
+	$1 $SAMPLES_DIR/sample${i}.bc sample${i}.c > $OUTPUT_DIR/sample${i}out.txt
+done
