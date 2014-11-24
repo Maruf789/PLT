284013621db577697558e7d3a32df48888045f4f
mwcu <wm19922009@gmail.com>
2014-11-23 22:32:32 -0500

    add comments in main

--- src/main.ml
+++ src/main.ml
@@ -1,18 +1,18 @@
 open Printf
 
+(* error reporting functions *)
 let loc_err lex_buf =
   let p = lex_buf.Lexing.lex_curr_p in
 		let tok = Lexing.lexeme lex_buf in
 		let line = p.Lexing.pos_lnum in
 		let cnum = p.Lexing.pos_cnum - p.Lexing.pos_bol + 1
 			         - String.length tok in
-		  sprintf
-			"token %s, line %d,%d"
-			tok line cnum
+		  sprintf "token %s, line %d,%d" tok line cnum
 
 let perror head err_msg =
   print_endline (head ^ ": " ^ err_msg)
 
+(* main function *)
 let main lex_buf =
   try
     let prog = Parser.program Scanner.token lex_buf in
@@ -25,6 +25,7 @@ let main lex_buf =
 		| Translate.Not_implemented -> perror "Translate error" (loc_err lex_buf)
 		| _ -> perror "Unkown error" (loc_err lex_buf)
 
+(* Shell interface *)
 let _ =
   if Array.length Sys.argv >= 2 then
     try
