df66a413da5bf29d7944e1d3b25c9859657e4766
mwcu <wm19922009@gmail.com>
2014-11-23 22:20:26 -0500

    more precise error message

--- src/main.ml
+++ src/main.ml
@@ -1,24 +1,29 @@
 open Printf
 
+let loc_err lex_buf =
+  let p = lex_buf.Lexing.lex_curr_p in
+		let tok = Lexing.lexeme lex_buf in
+		let line = p.Lexing.pos_lnum in
+		let cnum = p.Lexing.pos_cnum - p.Lexing.pos_bol + 1
+			         - String.length tok in
+		  sprintf
+			"token %s, line %d,%d"
+			tok line cnum
+
+let perror head err_msg =
+  print_endline (head ^ ": " ^ err_msg)
+
 let main lex_buf =
   try
     let prog = Parser.program Scanner.token lex_buf in
     let sprog = Scheck.check prog in
       Translate.compile sprog
   with
-      Scanner.Scanner_error x -> print_endline ("Scanner error: " ^ x)
-    | Sast.Bad_type x -> print_endline ("Sast error: " ^ x)
-		| Translate.Not_implemented -> print_endline "Translate error: not implemented"
-		| _ ->
-			let p = lex_buf.Lexing.lex_curr_p in
-			let tok = Lexing.lexeme lex_buf in
-			let line = p.Lexing.pos_lnum in
-			let cnum = p.Lexing.pos_cnum - p.Lexing.pos_bol + 1
-				         - String.length tok in
-			let msg = sprintf
-				"Error: token %s, line %d,%d"
-				tok line cnum in
-			print_endline msg
+      Scanner.Scanner_error x -> perror "Scanner error" x
+    | Parsing.Parse_error -> perror "Parser error" (loc_err lex_buf)
+    | Sast.Bad_type x -> perror "Sast error" x
+		| Translate.Not_implemented -> perror "Translate error" (loc_err lex_buf)
+		| _ -> perror "Unkown error" (loc_err lex_buf)
 
 let _ =
   if Array.length Sys.argv >= 2 then
