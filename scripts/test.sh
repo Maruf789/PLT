77fc7cb944c250abb9aa20af57db7fca08249012
Lan Yang <ireneyanglan@gmail.com>
2014-11-29 02:37:24 -0500

    most basic test cases done

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ SAMPLES_DIR=./test_samples
 OUTPUT_DIR=./test_outputs
 IDEAOUTPUT_DIR=./test_ideaoutputs
 
-for (( i =  0; i <= 24; i++))
+for (( i =  0; i <= 37; i++))
 do
 	$1 $SAMPLES_DIR/sample${i}.bc sample${i}.c > /dev/null\
 	 2> 		$OUTPUT_DIR/sample${i}out.txt 
