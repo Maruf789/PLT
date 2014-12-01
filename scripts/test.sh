6043de3573fb59c9426d2a0ac114c0f49646a026
Lan Yang <ireneyanglan@gmail.com>
2014-11-30 20:56:36 -0500

    test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,15 @@ SAMPLES_DIR=./test_samples
 OUTPUT_DIR=./test_outputs
 IDEAL_DIR=./test_ideaoutputs
 NU=/dev/null
+#DIF="git diff -b"
 DIF="diff -b -B"
 
-for (( i=0; i<3; i++ ))
+for (( i =  0; i <= 37; i++))
 do
-	$1 $SAMPLES_DIR/sample${i}.bc sample${i}.cpp
+	$1 $SAMPLES_DIR/sample${i}.bc sample${i}.c > $NU \
+	 2> 		$OUTPUT_DIR/sample${i}out.txt 
+	$DIF $IDEAL_DIR/sample${i}idea.txt $OUTPUT_DIR/sample${i}out.txt > $NU
+	if [ $? -eq 1 ]; then
+		echo "sample${i}.bc error"
+	fi
 done
-
