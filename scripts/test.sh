f908c9afa0341bf426fe5e3da20a561784f0a44b
Lan Yang <ireneyanglan@gmail.com>
2014-11-30 20:30:50 -0500

    modified test cases

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,16 @@
 
 SAMPLES_DIR=./test_samples
 OUTPUT_DIR=./test_outputs
-IDEAOUTPUT_DIR=./test_ideaoutputs
+IDEAL_DIR=./test_ideaoutputs
+NU=/dev/null
+#DIF="git diff -b"
+DIF="diff -b -B"
 
 for (( i =  0; i <= 37; i++))
 do
-	$1 $SAMPLES_DIR/sample${i}.bc sample${i}.c > /dev/null\
+	$1 $SAMPLES_DIR/sample${i}.bc sample${i}.c > $NU \
 	 2> 		$OUTPUT_DIR/sample${i}out.txt 
-	cmp -s $IDEAOUTPUT_DIR/sample${i}idea.txt $OUTPUT_DIR/sample${i}out.txt
+	$DIF $IDEAL_DIR/sample${i}idea.txt $OUTPUT_DIR/sample${i}out.txt > $NU
 	if [ $? -eq 1 ]; then
 		echo "sample${i}.bc error"
 	fi
