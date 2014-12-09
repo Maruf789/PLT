6f8c00efdfdec1f70d507c5e135a8c2babc02ac6
mwcu <wm19922009@gmail.com>
2014-12-09 18:09:49 -0500

    codegen v0.1

--- src/codegen.ml
+++ src/codegen.ml
@@ -10,18 +10,18 @@ open Printf
 
 (* Translate dtype to C++ types *)
 let tpt t = match t with
-      Int -> "int"
-    | Double -> "double"
-    | String -> "string"
-    | Bool -> "bool"
-    | IntMat -> "int_mat"
-    | DoubleMat -> "double_mat"
-    | StringMat -> "string_mat"
-    | Void -> "void"
+      Iint | Iint_array -> "int"
+    | Idouble | Idouble_array -> "double"
+    | Istring | Istring_array -> "string"
+    | Ibool -> "bool"
+    | Iint_mat -> "int_mat"
+    | Idouble_mat -> "double_mat"
+    | Istring_mat -> "string_mat"
+    | Ivoid -> "void"
 
-let trans_uop op = match op with Neg -> "-" | Not -> "!"
+let gen_uop op = match op with Neg -> "-" | Not -> "!"
 
-let trans_bop op = match op with
+let gen_bop op = match op with
     Plus -> "+" | Minus -> "-" | Times -> "*" | Divide -> "/"
   | Eq -> "==" | Neq -> "!=" | Lt -> "<" | Leq -> "<="
   | Gt -> ">" | Geq -> ">=" | And -> "&&" | Or -> "||"
@@ -29,86 +29,80 @@ let trans_bop op = match op with
 (* translate expr to string *)
 (* @ttbl: (int*sexpr list list) list - temporary variable table *)
 (* return: (int*sexpr) list * string - updated ttbl and target code *)
-let rec trans_expr ttbl exp = match exp with
-    _, SIntval x -> (sprintf " %d " x)
-  | _, SDoubleval x -> (sprintf "%f" x)
-  | _, SStringval x -> (" \"" ^ x ^ "\" ")
-  | _, SBoolval x -> if x then " true " else " false "
-  | _, SId x -> (" " ^ x ^ " ")
-  | _, SBinop (e1, b, e2) -> (sprintf " ( %s %s %s ) " (trans_expr e1) (trans_bop b) (trans_expr e2))
-  | _, SAssign (e1, e2) -> (sprintf "( %s = %s )" (trans_expr e1) (trans_expr e2))
-  | _, SUnaop (u, e) -> (sprintf "( %s %s )" (trans_uop u) (trans_expr e))
-  | _, SCall (s, el) -> (sprintf "( %s( %s ) )" s (trans_arg_list "," el))
-  | _, SMatSub (s, e1, e2) -> raise (Not_now "Matsub not implemented")
-  | t, SMatval (ell, nr, nc) -> let tid = List.length ttbl in ttbl@[tid, ell], (sprintf " %s(T_%d, %d, %d) " (tpt t) tid nr nc)
-and trans_arg_list ttbl sc el = match el with
+let rec gen_expr ttbl exp = match exp with
+    IIntval x -> (sprintf " %d " x)
+  | IDoubleval x -> (sprintf "%f" x)
+  | IStringval x -> (" \"" ^ x ^ "\" ")
+  | IBoolval x -> if x then " true " else " false "
+  | IId x -> (" " ^ x ^ " ")
+  | IBinop (e1, b, e2) -> (sprintf " ( %s %s %s ) " (gen_expr e1) (gen_bop b) (gen_expr e2))
+  | IAssign (e1, e2) -> (sprintf "( %s = %s )" (gen_expr e1) (gen_expr e2))
+  | IUnaop (u, e) -> (sprintf "( %s %s )" (gen_uop u) (gen_expr e))
+  | ICall (s, el) -> (sprintf "( %s( %s ) )" s (gen_arg_list "," el))
+  | IArray el-> raise (Not_now "Matsub not implemented")
+and gen_arg_list ttbl sc el = match el with
     [] -> ""
-  | [e] -> trans_expr ttbl e
-  | e1::e2::tl ->  sprintf "%s %s %s" (trans_expr ttbl e1) sc (trans_arg_list sc (e2::tl))
+  | [e] -> gen_expr ttbl e
+  | e1::e2::tl ->  sprintf "%s %s %s" (gen_expr ttbl e1) sc (gen_arg_list sc (e2::tl))
 
 (* translate variable definition list *)
-let rec trans_vardecs vars = match vars with
+let rec gen_vardecs vars = match vars with
     [] -> []
   | (t, s, e)::tl ->
-    (sprintf "%s %s = %s;" (tpt t) s (trans_expr e)) :: (trans_vardecs tl)
+    (sprintf "%s %s = %s;" (tpt t) s (gen_expr e)) :: (gen_vardecs tl)
 
 
 let gen_disp es = ("cout << " ^ es ^ " << endl;")
 
 (* translate statement list *)
-let rec trans_elifs elifs = match elifs with (* translate a list of elif *)
-    [] -> []
-  | hd::tl -> (trans_condstmts "elif" hd) @ (trans_elifs tl)
-and trans_condstmts sc cs =
-  [sprintf "%s (%s) { " sc (trans_expr cs.scond)] @ (trans_stmts cs.sstmts)
-and trans_stmts tid stmts = match stmts with
+let rec gen_stmts stmts = match stmts with
     [] -> []
   | hd::tl -> ( match hd with
-        SEmpty -> [";"]
-      | SExpr e -> [(trans_expr e) ^ " ;"]
-      | SReturn e -> [sprintf "return %s ;" (trans_expr e)]
-      | SIf (cs, csl, sl) -> (trans_condstmts "if" cs) @ (trans_elifs csl) @ (trans_stmts sl)
-      | SCntFor (_, _, _) -> raise (Not_now "CntFor not implemented")
-      | SCndFor _ -> raise (Not_now "CndFor not implemented")
-      | SDisp e -> (let es = trans_expr e in [gen_disp es])
-      | SContinue -> ["continue;"]
-      | SBreak -> ["break;"]
-    ) @ (trans_stmts tl)
+        IEmpty -> [";"]
+      | IExpr e -> [(gen_expr e) ^ " ;"]
+      | IReturn e -> [sprintf "return %s ;" (gen_expr e)]
+      | IIfHead e -> [](*(gen_condstmts "if" cs) @ (gen_elifs csl) @ (gen_stmts sl)*)
+      | IElseIf e -> [](*(gen_condstmts "if" cs) @ (gen_elifs csl) @ (gen_stmts sl)*)
+      | IElse e -> [](*(gen_condstmts "if" cs) @ (gen_elifs csl) @ (gen_stmts sl)*)
+      | IForHead e -> [](*(gen_condstmts "if" cs) @ (gen_elifs csl) @ (gen_stmts sl)*)
+      | IWhileHead e -> [](*(gen_condstmts "if" cs) @ (gen_elifs csl) @ (gen_stmts sl)*)
+      | IDisp e -> (let es = gen_expr e in [gen_disp es])
+      | IContinue -> ["continue;"]
+      | IBreak -> ["break;"]
+    ) @ (gen_stmts tl)
 
 
-let rec trans_args sc args = match args with
+let rec gen_args sc args = match args with
     [] -> ""
   | [a] -> sprintf "%s %s" (tpt a.vtype) (a.vname)
-  | a::b::tl -> (sprintf "%s %s%s " (tpt a.vtype) a.vname sc) ^ (trans_args sc (b::tl))
-let rec trans_fundefs fundefs = match fundefs with
+  | a::b::tl -> (sprintf "%s %s%s " (tpt a.vtype) a.vname sc) ^ (gen_args sc (b::tl))
+let rec gen_fundefs fundefs = match fundefs with
     [] -> []
-  | hd::tl -> (if hd.sbody=[] then 
-               ([sprintf "%s %s(%s) {" (tpt hd.sreturn) hd.sfname (trans_args "," hd.sargs)]
-               @(trans_vardecs hd.slocals)@(trans_stmts hd.sbody))
-               else ([sprintf "%s %s(%s) ;" (tpt hd.sreturn) hd.sfname (trans_args "," hd.sargs)])
-              )@(trans_fundefs tl)
+  | hd::tl -> (if hd.ibody != [] then
+               ([sprintf "%s %s(%s) {" (tpt hd.ireturn) hd.ifname (gen_args "," hd.sargs)]
+               @(gen_stmts hd.ibody)@ "}")
+               else ([sprintf "%s %s(%s) ;" (tpt hd.itype) hd.ifname (gen_args "," hd.sargs)])
+              )@(gen_fundefs tl)
 
 
 let compile prg =
-  let head_lines = 
-    ["#include \"buckcal_types.h\""; "int_mat GT_int_mat;"; 
-     "string_mat GT_string_mat;"; "double_mat GT_double_mat;"] 
+  let head_lines =
+    ["#include \"buckcal_types.h\""] 
   in
   let func_lines =
     let funs = prg.spfuns in
-    trans_fundefs funs
+    gen_fundefs funs
   in
   let var_lines =
     let vars = prg.spvars in
-    trans_vardecs vars
+    gen_vardecs vars
   in
   let stm_lines =       (* statements *)
     let stms = prg.spstms in
-    trans_stmts 0 stms
+    gen_stmts stms
   in
   let all = head_lines @ func_lines
             @ ["int main() {"] @ var_lines @ stm_lines
             @["return 0;"; "}"] 
   in
   List.iter print_endline all
-
