02d693a62e222a6f550da3376f50f7a0e08597ed
mwcu <wm19922009@gmail.com>
2014-12-10 21:30:24 -0500

    fix T naming conflict

--- src/translate.ml
+++ src/translate.ml
@@ -38,58 +38,58 @@ let smat_to_cnsr m = match m with
 
 (* @isl: irstmt list *)
 (* return : irstmt list * irexpr *)
-let rec trans_expr isl exp = match exp with
+let rec trans_expr tid isl exp = match exp with
     _, SIntval x -> isl, (IIntval x)
   | _, SDoubleval x -> isl, (IDoubleval x)
   | _, SStringval x -> isl, (IStringval x)
   | _, SBoolval x -> isl, (IBoolval x)
   | _, SId x -> isl, (IId x)
-  | _, SBinop (e1, b, e2) -> (let isl, ie1 = trans_expr isl e1 in
-                              let isl, ie2 = trans_expr isl e2 in
+  | _, SBinop (e1, b, e2) -> (let isl, ie1 = trans_expr tid isl e1 in
+                              let isl, ie2 = trans_expr tid isl e2 in
                               (isl, (IBinop (ie1, b,ie2))))
-  | _, SAssign (e1, e2) -> (let isl, ie1 = trans_expr isl e1 in
-                            let isl, ie2 = trans_expr isl e2 in
+  | _, SAssign (e1, e2) -> (let isl, ie1 = trans_expr tid isl e1 in
+                            let isl, ie2 = trans_expr tid isl e2 in
                             (isl, (IAssign (ie1, ie2))))
-  | _, SUnaop (u, e) -> (let isl, ie = trans_expr isl e in
+  | _, SUnaop (u, e) -> (let isl, ie = trans_expr tid isl e in
                          (isl, (IUnaop (u, ie))))
-  | _, SCall (s, el) -> (let is1, iesl = trans_arglist isl el in
+  | _, SCall (s, el) -> (let is1, iesl = trans_arglist tid isl el in
                          (isl, ICall (s, iesl)))
-  | _, SMatSub (s, e1, e2) -> (let isl, ie1 = trans_expr isl e1 in
-                               let isl, ie2 = trans_expr isl e2 in
+  | _, SMatSub (s, e1, e2) -> (let isl, ie1 = trans_expr tid isl e1 in
+                               let isl, ie2 = trans_expr tid isl e2 in
                                (isl, (trans_matsub s ie1 ie2)))
   | t, SMatval (ell, nr, nc) -> (
       let arr = trans_matval ell in
       let ta = smat_to_array t in
-      let tname = sprintf "T_%d" (List.length isl) in
+      let tname = sprintf "T_%d_%d" tid (List.length isl) in
       let isl = isl@[IVarDec (ta, tname, arr)] in
       let ex = ICall ((smat_to_cnsr t), [IId tname; IIntval nr; IIntval nc]) in
       isl, ex)
-and trans_arglist is1 el = match el with
+and trans_arglist tid is1 el = match el with
     [] -> is1, []
-  | e::tl -> (let isl, ie = trans_expr is1 e in
-              let isl, itl = trans_arglist isl tl in
+  | e::tl -> (let isl, ie = trans_expr tid is1 e in
+              let isl, itl = trans_arglist tid isl tl in
               isl, ie::itl)
 and trans_matval ell = (* matrix element should not generate extra irstmt *)
   let el = List.flatten ell in
-  let irel = List.map (fun x -> snd (trans_expr [] x)) el in
+  let irel = List.map (fun x -> snd (trans_expr 0 [] x)) el in
   (IArray irel)
 
 (* translate variable definition list *)
-let rec trans_vardecs vars = match vars with
+let rec trans_vardecs tid vars = match vars with
     [] -> []
   | (t, s, e)::tl -> (
       let it = ipt t in
-      let isl, ie = trans_expr [] e in
+      let isl, ie = trans_expr tid [] e in
       (isl@[IVarDec (it, s, ie)])
-    ) @ (trans_vardecs tl)
+    ) @ (trans_vardecs (tid + 1) tl)
 
 
-let rec trans_stmts tid stmts = match stmts with
+let rec trans_stmts tid stmts = (*print_int tid;*) match stmts with
     [] -> []
   | hd::tl -> ( match hd with
       | SEmpty -> [IEmpty]
-      | SExpr e -> let isl, ie = trans_expr [] e in isl@[IExpr ie]
-      | SReturn e -> let isl, ie = trans_expr [] e in isl@[IReturn ie]
+      | SExpr e -> let isl, ie = trans_expr tid [] e in isl@[IExpr ie]
+      | SReturn e -> let isl, ie = trans_expr tid [] e in isl@[IReturn ie]
       | SIf (cs, csl, sl) ->
         let part1 =
           let isl0, ie, is = trans_condstmt tid [] cs in
@@ -108,12 +108,12 @@ let rec trans_stmts tid stmts = match stmts with
         ( [fh] @ lbody @ [IBlockEnd] )
       | SCndFor cs -> let isl0, ie, is = trans_condstmt tid [] cs in
         (isl0 @ [IWhileHead ie] @ is @ [IBlockEnd])
-      | SDisp e -> let isl, ie = trans_expr [] e in isl@[IDisp ie]
+      | SDisp e -> let isl, ie = trans_expr tid [] e in isl@[IDisp ie]
       | SContinue -> [IContinue]
       | SBreak -> [IBreak]
     ) @ (trans_stmts (tid + 1) tl)
 and trans_condstmt tid isl cs =
-  let isl0, iec = trans_expr isl cs.scond in
+  let isl0, iec = trans_expr tid isl cs.scond in
   let ies = trans_stmts (tid + 1) cs.sstmts in
   (isl0, iec, ies)
 and trans_condstmts tid condstmtlist = match condstmtlist with 
@@ -138,7 +138,7 @@ let rec trans_args args = match args with
 let rec trans_fundefs fundefs = match fundefs with
     [] -> []
   | hd::tl -> (
-      let ss_v = trans_vardecs hd.slocals in
+      let ss_v = trans_vardecs 0 hd.slocals in
       let ss_s = trans_stmts 0 hd.sbody in
       { ireturn = (ipt hd.sreturn); ifname = hd.sfname;
         iargs = (trans_args hd.sargs); ibody = (ss_v@ss_s) }
@@ -152,7 +152,7 @@ let translate prg =
   in
   let var_lines =
     let vars = prg.spvars in
-    trans_vardecs vars
+    trans_vardecs 0 vars
   in
   let stmt_lines =
     let stmts = prg.spstms in
