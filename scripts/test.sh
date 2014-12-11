4e85d78b1d284c8454eada9fbac926295375eac9
Lan Yang <ireneyanglan@gmail.com>
2014-12-10 19:57:07 -0500

    modify test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-SAMPLES_DIR=./test_samples
+GOOD_DIR=./test_samples_good
+BAD_DIR=./test_samples_bad
 OUTPUT_DIR=./test_outputs
 IDEAL_DIR=./test_ideaoutputs
 NU=/dev/null
@@ -14,20 +15,30 @@ cp ../src/buckcal_types.h $CFILE_DIR
 cp ../src/c++/buckcal_mat.cpp $CFILE_DIR
 cp ../src/c++/buckcal_mat.hpp $CFILE_DIR
 
-# compare <outputfile> <idealoutputfile>
-for (( i =  38; i <= 38; i++))
+# run good cases
+for (( i =  26; i <= 26; i++))
 do
-	$1 $SAMPLES_DIR/sample${i}.bc sample${i}.c > $NU \
-	 2> 		$OUTPUT_DIR/sample${i}out.txt 
-	$DIF $IDEAL_DIR/sample${i}idea.txt $OUTPUT_DIR/sample${i}out.txt > $NU
-	if [ $? -eq 1 ]; then
-		echo "sample${i}.bc error"
+	$1 $GOOD_DIR/sample${i}.bc sample${i}.c > $NU \
+	 2> 		$OUTPUT_DIR/goodsample${i}out.txt 
+	#$DIF $IDEAL_DIR/sample${i}idea.txt $OUTPUT_DIR/sample${i}out.txt > $NU
+	if [ -s $OUTPUT_DIR/goodsample${i}out.txt ]; then
+		echo "good sample${i}.bc error"
 	fi
-# run <args>
-	$1 $SAMPLES_DIR/sample${i}.bc > $CFILE_DIR/sample${i}.cpp \
-	2>>		$OUTPUT_DIR/sample${i}out.txt
+	$1 $GOOD_DIR/sample${i}.bc > $CFILE_DIR/sample${i}.cpp \
+	2>>		$OUTPUT_DIR/goodsample${i}out.txt
 	if [ -s $CFILE_DIR/buckcal_mat.cpp ]; then
 		g++ $CFILE_DIR/sample${i}.cpp $CFILE_DIR/buckcal_mat.cpp -o $BINFILE/sample${i}.bin \
-		2>>		$OUTPUT_DIR/sample${i}out.txt
+		2>>		$OUTPUT_DIR/goodsample${i}out.txt
+	fi
+done
+
+# run bad cases
+for (( i =  0; i <= 0; i++))
+do
+	$1 $GOOD_DIR/sample${i}.bc sample${i}.c > $NU \
+	 2> 		$OUTPUT_DIR/badsample${i}out.txt 
+	$DIF $IDEAL_DIR/sample${i}idea.txt $OUTPUT_DIR/badsample${i}out.txt > $NU
+	if [ $? -eq 1 ]; then
+		echo "bad sample${i}.bc error"
 	fi
 done
