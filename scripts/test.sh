1dcf8977d67e93f7775867fec2b69fe18c1ddffd
Lan Yang <ireneyanglan@gmail.com>
2014-12-10 23:35:48 -0500

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,7 +19,7 @@ cp ../src/c++/buckcal_lib.cpp $CFILE_DIR
 #g++ -c $CFILE_DIR/buckcal_mat.cpp -o $CFILE_DIR/buckcal_mat.o
 
 # run good cases
-for (( i =  0; i <= 23; i++))
+for (( i =  0; i <= 25; i++))
 do
 	$1 $GOOD_DIR/sample${i}.bc sample${i}.c > $NU \
 	 2> 		$OUTPUT_DIR/goodsample${i}out.txt 
