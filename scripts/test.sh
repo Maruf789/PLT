123a52068c4ca8df07ced7c4a8166472dc6ec450
Lan Yang <ireneyanglan@gmail.com>
2014-12-12 00:11:36 -0500

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,7 +19,7 @@ cp ../src/c++/buckcal_lib.cpp $CFILE_DIR
 #g++ -c $CFILE_DIR/buckcal_mat.cpp -o $CFILE_DIR/buckcal_mat.o
 
 # run good cases
-for (( i =  35; i <= 36; i++))
+for (( i =  0; i <= 36; i++))
 do
 	$1 $GOOD_DIR/sample${i}.bc $CFILE_DIR/sample${i}.cpp \
 	2>		$OUTPUT_DIR/goodsample${i}out.txt
@@ -33,7 +33,7 @@ do
 done
 
 # run bad cases
-for (( i =  0; i <= 41; i++))
+for (( i =  0; i <= 42; i++))
 do
 	$1 $BAD_DIR/sample${i}.bc > $NU \
 	 2> 		$OUTPUT_DIR/badsample${i}out.txt 
