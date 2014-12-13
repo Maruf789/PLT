2394f641cdd52c20c3f4b9104ee1b1f88a0170cd
Lingyuan He <lh2710@columbia.edu>
2014-12-13 12:35:25 -0800

    get/setrow and init_mat

--- src/c++/buckcal_mat.hpp
+++ src/c++/buckcal_mat.hpp
@@ -100,55 +100,55 @@ std::ostream & operator << (std::ostream &sys, const int_mat &in);
 std::ostream & operator << (std::ostream &sys, const double_mat &in);
 std::ostream & operator << (std::ostream &sys, const string_mat &in);
 
-/* library calls */
-int_mat range(int x, int y) ;
-int_mat ranger(int x, int y) ;
-int rows(int_mat mx) ;
-int rows(double_mat mx) ;
-int rows(string_mat mx) ;
-int cols(int_mat mx) ;
-int cols(double_mat mx) ;
-int cols(string_mat mx) ;
-int_mat rowcat(int_mat mx1, int_mat mx2) ;
-double_mat rowcat(double_mat mx1, double_mat mx2) ;
-string_mat rowcat(string_mat mx1, string_mat mx2) ;
-int_mat colcat(int_mat mx1, int_mat mx2) ;
-double_mat colcat(double_mat mx1, double_mat mx2) ;
-string_mat colcat(string_mat mx1, string_mat mx2) ;
-void colunit(double_mat mx, string_mat u) ;
-void rowname(int_mat &mx, string_mat n) ;
-void rowname(double_mat &mx, string_mat n) ;
-void rowname(string_mat &mx, string_mat n) ;
-void colname(int_mat &mx, string_mat n) ;
-void colname(double_mat &mx, string_mat n) ;
-void colname(string_mat &mx, string_mat n) ;
-void init_mat_col(int_mat mx, string_mat nc) ;
-void init_mat_col(double_mat mx, string_mat nc) ;
-void init_mat_col(string_mat mx, string_mat nc) ;
-void init_mat_size(int_mat mx, int nrow, int ncol) ;
-void init_mat_size(double_mat mx, int nrow, int ncol) ;
-void init_mat_size(string_mat mx, int nrow, int ncol) ;
-int_mat addrows(int_mat mx, int_mat dr, string_mat drn) ;
-double_mat addrows(double_mat mx, double_mat dr, string_mat drn) ;
-string_mat addrows(string_mat mx, string_mat dr, string_mat drn) ;
-std::string string_of_int(int x) ;
-std::string string_of_double(double x) ;
-int int_of_string(std::string x) ;
-double double_of_string(std::string x) ;
-int_mat mat_int_of_string(string_mat x) ;
-double_mat mat_double_of_string(string_mat x) ;
-int_mat mat_int_of_double(double_mat x) ;
-double_mat mat_double_of_int(int_mat x) ;
-string_mat mat_string_of_int(int_mat x) ;
-string_mat mat_string_of_double(double_mat x) ;
-int strlen(std::string x) ;
-std::string slice(std::string x, int_mat idx) ;
-double abs(double x) ;
-double_mat sum_row(double_mat mx) ;
-double_mat sum_col(double_mat mx) ;
-double_mat avg_row(double_mat mx) ;
-double_mat avg_col(double_mat mx) ;
-double_mat var_row(double_mat mx) ;
-double_mat var_col(double_mat mx) ;
+/* data conversion */
+std::string string_of_int(int x);
+std::string string_of_double(double x);
+int int_of_string(std::string x);
+double double_of_string(std::string x);
+int_mat mat_int_of_string(string_mat x);
+double_mat mat_double_of_string(string_mat x);
+string_mat mat_string_of_int(int_mat x);
+string_mat mat_string_of_double(double_mat x);
+int_mat mat_int_of_double(double_mat x);
+double_mat mat_double_of_int(int_mat x);
 
+/* row and column concatenation */
+int_mat rowcat(int_mat mx1, int_mat mx2);
+double_mat rowcat(double_mat mx1, double_mat mx2);
+string_mat rowcat(string_mat mx1, string_mat mx2);
+int_mat rowcat(int_mat mx1, double_mat mx2);
+double_mat rowcat(double_mat mx1, int_mat mx2);
+
+int_mat colcat(int_mat mx1, int_mat mx2);
+double_mat colcat(double_mat mx1, double_mat mx2);
+string_mat colcat(string_mat mx1, string_mat mx2);
+int_mat colcat(int_mat mx1, double_mat mx2);
+double_mat colcat(double_mat mx1, int_mat mx2);
+
+/* row and column names */
+void rowname(int_mat &mx, string_mat n);
+void rowname(double_mat &mx, string_mat n);
+void rowname(string_mat &mx, string_mat n);
+
+void colname(int_mat &mx, string_mat n);
+void colname(double_mat &mx, string_mat n);
+void colname(string_mat &mx, string_mat n);
+
+/* string operations */
+int strlen(std::string x);
+std::string slice(std::string x, int_mat idx);
+
+/* get or set row/col */
+int_mat getrow(int_mat mat, int r);
+double_mat getrow(double_mat mat, int r);
+string_mat getrow(string_mat mat, int r);
+
+void setrow(int_mat mat, int r, int_mat set);
+void setrow(double_mat mat, int r, double_mat set);
+void setrow(string_mat mat, int r, string_mat set);
+
+/* init matrixes */
+int_mat init_mat(int r, int c, int init);
+double_mat init_mat(int r, int c, double init);
+string_mat init_mat(int r, int c, std::string init);
 #endif
